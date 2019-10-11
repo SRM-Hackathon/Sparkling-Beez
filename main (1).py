145d7277f98a0e811a264f74d483f3d947fdc76c
Anandha Balaji A <56443080+Rubalaji@users.noreply.github.com>
2019-10-11 23:13:15 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,17 @@
+from random import randint
+while (1!=0):
+    i = input("Hack Code: ")
+    x = randint(-10000,10000)
+    if (x==i):    
+        print("Hacked")
+    if (x!=i):
+        print("UnHackable")
+    print(x)
+    
+            #Hack Code: 5                                                                                                                    
+            #UnHackable                                                                                                                      
+            #606                                                                                                                             
+            #Hack Code: 859                                                                                                                  
+            #UnHackable                                                                                                                      
+            #-5969                                                                                                                           
+            #Hack Code:
