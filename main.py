c7a65a82b668980f8ca64659b061a1f9e43a9fe2
Anandha Balaji A <56443080+Rubalaji@users.noreply.github.com>
2019-10-12 11:23:11 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+from random import randint
+while (1!=0):
+    print("Hai Hackster!")
+    print("I will set your codes at Fire!")
+    print("The Source Codes are hidden in a 4D point. Enter its Co-Ordinates!")
+    i = input()
+    j = input()
+    k = input()
+    l = input()
+    t = randint(-10000,10000)
+    x = randint(-10000,10000)
+    y = randint(-10000,10000)
+    z = randint(-10000,10000)
+    
+    if (t==i):    
+        if (x==j):
+            if (y==k):
+                if (z==l):
+                     print("Block busted!")
+    if (t!=i or x!=j or y!=k or z!=l):    
+        print("Block can't be Trooked!")
+    print(t,x,y,z)
+    
+    # Hai Hackster!                                                                                                                 
+    # I will set your codes at Fire!                                                                                                
+    # The Source Codes are hidden in a 4D point. Enter its Co-Ordinates!                                                            
+        #2                                                                                                                             
+        #5                                                                                                                             
+        #6                                                                                                                             
+        #9 
+    # Block can't be Trooked!                                                                                                       
+        #1165 8095 -7044 -6470
