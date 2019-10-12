f2c704ea6ec11bb325f42ec54e2c180aa7dd1ef3
Anandha Balaji A <56443080+Rubalaji@users.noreply.github.com>
2019-10-12 09:21:27 +0530

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,31 @@
+from random import randint
+while (1!=0):
+    print("Vangada Selfie Pullingo!")
+    print("I will set your codes at Fire!")
+    i = input("Hack_Me @ If You dare: ")
+    j = input("Hack_Me @ If You dare: ")
+    k = input("Hack_Me @ If You dare: ")
+    l = input("Hack_Me @ If You dare: ")
+    t = randint(-10000,10000)
+    x = randint(-10000,10000)
+    y = randint(-10000,10000)
+    z = randint(-10000,10000)
+    
+    if (t==i):    
+        if (x==j):
+            if (y==k):
+                if (z==l):
+                     print("Hacked!")
+    if (t!=i or x!=j or y!=k or z!=l):    
+        print("Ninja Trook!")
+    #print(t,x,y,z)
+        
+        #Vangada Selfie Pullingo!                                                                                                      
+        #I will set your codes at Fire!                                                                                                
+        #Hack_Me @ If You dare: 45                                                                                                     
+        #Hack_Me @ If You dare: 46                                                                                                     
+        #Hack_Me @ If You dare: -895                                                                                                   
+        #Hack_Me @ If You dare: 23                                                                                                     
+        #Ninja Trook!
+            
+           
